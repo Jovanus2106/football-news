29c1959e7ee196eb74fc418430adbd2e8a609017
Jovanus Irwan <jovanusirwan@gmail.com>
2025-09-10 17:18:31 +0700

    tugas 2 bener selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jovanus-irwan-footballnews.pbp.cs.ui.ac.id"]
-CSRF_TRUSTED_ORIGINS = ["https://jovanus-irwan-footballnews.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["https://jovanus-irwan-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
